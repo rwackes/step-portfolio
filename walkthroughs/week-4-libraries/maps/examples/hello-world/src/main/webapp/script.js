22aefbbc166ea16c448894e2f0f57e5ba4e20287
Rhiannan Wackes <wackesrhiannan@gmail.com>
2020-06-11 22:57:10 +0000

    Edit walkthroughs to include my API key and so map var was declared outside function, which solved error of map not loading.

--- walkthroughs/week-4-libraries/maps/examples/hello-world/src/main/webapp/script.js
+++ walkthroughs/week-4-libraries/maps/examples/hello-world/src/main/webapp/script.js
@@ -13,8 +13,9 @@
 // limitations under the License.
 
 /** Creates a map and adds it to the page. */
+var map;
 function createMap() {
-  const map = new google.maps.Map(
+  map = new google.maps.Map(
       document.getElementById('map'),
       {center: {lat: 37.422, lng: -122.084}, zoom: 16});
 }
